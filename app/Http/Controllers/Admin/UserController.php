cb11ff29e1c3404288f6b23253d192e822d8cbb7
Y7murata <y7sz05@gmail.com>
2024-06-19 14:02:11 +0900

    Herokuへのデプロイ準備

---
+++ app/Http/Controllers/Admin/UserController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    //会員一覧ページ　
+    //検索ボックスに入力されたキーワードが存在する場合は、氏名またはフリガナで部分一致検索を行う
+    DB::table('users')->select('name','kana')->get();
+    $users = User::where("name", "kana")->get();
+
+    //検索ボックスに入力されたキーワード
+
+    //取得したデータ（$users）の総数
+}
