54454eb5d1401831eee621d2981d574e09854610
Y7murata <y7sz05@gmail.com>
2024-06-28 14:15:16 +0900

    “店舗の管理機能の追加”

---
+++ app/Http/Controllers/Admin/RestaurantController.php
@@ -0,0 +1,197 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Restaurant;
+
+class RestaurantController extends Controller
+{
+
+    // -----
+    // index アクション
+    // ------
+     public function index(Request $request) {
+        // 検索ボックスに入力されたキーワードを取得する
+        $keyword = $request->input('keyword');
+
+        // キーワードが存在すれば検索を行い、そうでなければ全件取得する
+        if ($keyword) {
+            $restaurants = Restaurant::where('name', 'like', "%{$keyword}%")->paginate(15);
+        } else {
+            $restaurants = Restaurant::paginate(15);
+        }
+
+        $total = $restaurants->total();
+
+        return view('admin.restaurants.index', compact('restaurants', 'keyword', 'total'));
+    }
+
+    // ------
+    // show アクション
+    // ------
+
+    public function show(Restaurant $restaurant) {
+        return view('admin.restaurants.show', compact('restaurant'));
+    }
+
+    // ------
+    // create アクション
+    // ------
+
+    public function create() {
+        return view('admin.restaurants.create');
+    }
+
+    // ------
+    // store アクション
+    // ------
+    /*public function store(Request $request) {
+
+        //バリデーションの設定
+        $request->validate([
+            'name' => 'required',
+            'image' => 'image|max:2048',
+            'description' => 'required',
+            'lowest_price' => 'required|numeric|min:0|lte:highest_price',
+            'highest_price' => 'required|numeric|min:0|gte:lowest_price',
+            'postal_code' => 'required|digits:7',
+            'address' => 'required',
+            'opening_time' => 'required|before:closing_time',
+            'closing_time' => 'required|after:opening_time',
+            'seating_capacity' => 'required|numeric|min:0',
+        ]);
+
+         // Restaurant クラスの初期化
+         $restaurant = new Restaurant();
+
+     //処理内容
+     //フォームからの入力値を取得
+        $restaurant->name = $request->input('name');
+        // アップロードされたファイル（name="image"）が存在すれば処理を実行する
+        if ($request->hasFile('image')) {
+        // アップロードされたファイル（name="image"）をstorage/app/public/restaurantsフォルダに保存し、戻り値（ファイルパス）を変数$imageに代入する
+        $image = $request->file('image')->store('public/restaurants');
+        // ファイルパスからファイル名のみを取得し、Restaurantインスタンスのimageプロパティに代入する
+        $restaurant->image = basename($image);
+        } else {
+            $restaurant->image = '';
+        }
+        $restaurant->description = $request->input('description');
+        $restaurant->lowest_price = $request->input('lowest_price');
+        $restaurant->highest_price = $request->input('highest_price');
+        $restaurant->postal_code = $request->input('postal_code');
+        $restaurant->address = $request->input('address');
+        $restaurant->opening_time = $request->input('opening_time');
+        $restaurant->closing_time = $request->input('closing_time');
+        $restaurant->seating_capacity = $request->input('seating_capacity');
+        $restaurant->save();
+
+　　 //リダイレクト先とフラッシュメッセージ
+    return redirect()->route('admin.restaurants.index')->with('flash_message', '店舗を登録しました。');
+    }*/
+
+    public function store(Request $request) {
+        $request->validate([
+            'name' => 'required',
+            'image' => 'image|max:2048',
+            'description' => 'required',
+            'lowest_price' => 'required|numeric|min:0|lte:highest_price',
+            'highest_price' => 'required|numeric|min:0|gte:lowest_price',
+            'postal_code' => 'required|digits:7',
+            'address' => 'required',
+            'opening_time' => 'required|before:closing_time',
+            'closing_time' => 'required|after:opening_time',
+            'seating_capacity' => 'required|numeric|min:0',
+        ]);
+
+        $restaurant = new Restaurant();
+        $restaurant->name = $request->input('name');
+        if ($request->hasFile('image')) {
+            $image = $request->file('image')->store('public/restaurants');
+            $restaurant->image = basename($image);
+        } else {
+            $restaurant->image = '';
+        }
+        $restaurant->description = $request->input('description');
+        $restaurant->lowest_price = $request->input('lowest_price');
+        $restaurant->highest_price = $request->input('highest_price');
+        $restaurant->postal_code = $request->input('postal_code');
+        $restaurant->address = $request->input('address');
+        $restaurant->opening_time = $request->input('opening_time');
+        $restaurant->closing_time = $request->input('closing_time');
+        $restaurant->seating_capacity = $request->input('seating_capacity');
+        $restaurant->save();
+
+        return redirect()->route('admin.restaurants.index')->with('flash_message', '店舗を登録しました。');
+    }
+
+
+    // ------
+    // edit アクション
+    // ------
+    public function edit(Restaurant $restaurant) {
+     // ここにビューへの戻りを追加
+        return view('admin.restaurants.edit', compact('restaurant'));
+    }
+
+    // ------
+    // update アクション
+    // ------
+    public function update(Request $request, Restaurant $restaurant) {
+
+    //フォームからの入力値を取得
+     //バリデーションの設定
+        $request->validate([
+          'name' => 'required',
+          'image' => 'image|max:2048',
+          'description' => 'required',
+          'lowest_price' => 'required|numeric|min:0|lte:highest_price',
+          'highest_price' => 'required|numeric|min:0|gte:lowest_price',
+          'postal_code' => 'required|digits:7',
+          'address' => 'required',
+          'opening_time' => 'required|before:closing_time',
+          'closing_time' => 'required|after:opening_time',
+          'seating_capacity' => 'required|numeric|min:0',
+        ]);
+
+        //処理の内容
+        // アップロードされたファイル（name="image"）が存在すれば処理を実行する
+        if ($request->hasFile('image')) {
+            // アップロードされたファイル（name="image"）をstorage/app/public/restaurantsフォルダに保存し、戻り値（ファイルパス）を変数$imageに代入する
+                $image_path = $request->file('image')->store('public/restaurants');
+            // ファイルパスからファイル名のみを取得し、Restaurantインスタンスのimageプロパティに代入する
+                $restaurant->image = basename($image_path);
+            }
+
+         $restaurant->name = $request->input('name');
+         $restaurant->description = $request->input('description');
+         $restaurant->lowest_price = $request->input('lowest_price');
+         $restaurant->highest_price = $request->input('highest_price');
+         $restaurant->postal_code = $request->input('postal_code');
+         $restaurant->address = $request->input('address');
+         $restaurant->opening_time = $request->input('opening_time');
+         $restaurant->closing_time = $request->input('closing_time');
+         $restaurant->seating_capacity = $request->input('seating_capacity');
+         $restaurant->save();
+
+    //リダイレクト先とフラッシュメッセージ
+        // 登録しましたというメッセージと共にビューへのリダイレクトを追加しましょう
+        return redirect()->route('admin.restaurants.show', $restaurant)->with('flash_message', '店舗を編集しました。');
+    }
+
+    // ------
+    // destroy アクション
+    // ------
+    public function destroy(Restaurant $restaurant) {
+          
+         //処理の内容 削除処理
+         $restaurant->delete();
+
+         // リダイレクト先とフラッシュメッセージ
+       return redirect()->route('admin.restaurants.index')->with('flash_message', '店舗を登録しました。');
+     }
+
+    }
+
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\Admin;
 //Userコントローラの宣言
 use App\Http\Controllers\UserController;
 
+//Restaurantコントローラの宣言
+use App\Http\Controllers\Admin\RestaurantController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,8 +27,14 @@ Route::get('/', function () {
 require __DIR__.'/auth.php';
 
 Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => 'auth:admin'], function () {
+
+    // HOME
     Route::get('home', [Admin\HomeController::class, 'index'])->name('home');
-// ここをみて、テスト実行の際、トップページに遷移するようになる
+
+    // User
     Route::resource('users', Admin\UserController::class)->only(['index', 'show']);
+
+    //　Restaunrantのルーティングを以下に１行でまとめて追加
+    Route::resource('restaurants', Admin\RestaurantController::class);
 
 });
