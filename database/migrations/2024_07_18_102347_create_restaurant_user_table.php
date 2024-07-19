582f92b0beb5f76c82b274eea18482902b96d10f
Y7murata <y7sz05@gmail.com>
2024-07-19 19:39:47 +0900

    “会員側のお気に入り機能の追加”

--- database/migrations/2024_07_18_102347_create_restaurant_user_table.php
+++ database/migrations/2024_07_18_102347_create_restaurant_user_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('restaurant_users', function (Blueprint $table) {
+        Schema::create('restaurant_user', function (Blueprint $table) {
             $table->id();
             $table->foreignId('restaurant_id')->constrained()->cascadeOnDelete();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('restaurant_users');
+        Schema::dropIfExists('restaurant_user');
     }
 };
